fe3ae326391bde35bc6e437aea3e793dfdb92bcb
Michael Cullan <mjcullan@gmail.com>
2021-01-13 15:47:51 -0500

    fixed quotes in setup

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 mkdir -p ~/.streamlit/
-echo “\
+
+echo "\
 [general]\n\
-email = \”mjcullan@gmail.com\”\n\
-“ > ~/.streamlit/credentials.toml
-echo “\
+email = \"mjcullan@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
-“ > ~/.streamlit/config.toml
+" > ~/.streamlit/config.toml
